58152a220ec29dcc9c014b2a150edbc2c56fc88d
alu0101333823 <alu0101333823@ull.edu.es>
2022-03-30 08:44:37 +0000

    clase randomNumber

--- tests/randomNumber.spec.ts
+++ tests/randomNumber.spec.ts
@@ -3,11 +3,6 @@ import {expect} from 'chai';
 
 describe('add function tests', () => {
   it('add(1, 8) returns value 9', () => {
-  });
-
-  it('add(-1, 8) returns value 7', () => {
-  });
-
-  it('add(1.2, 3.5) returns value 4.7', () => {
+    expect(true).to.be.true;
   });
 });
